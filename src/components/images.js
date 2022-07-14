7263c5f79ce210f98f058cb3b04c7068f9d7b89f
Edward Valentine <eddieval2022@gmail.com>
2022-07-14 23:38:10 +0100

    components

---
+++ src/components/images.js
@@ -0,0 +1,11 @@
+const Image = ({ author, url }) => {
+  return (
+    <div>
+      <h2>{author}</h2>
+
+      <img src={url} alt={`random grab from author ${author}`} />
+    </div>
+  );
+};
+
+export default Image;
