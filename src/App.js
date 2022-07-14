540d8b37c8b9091a3b11560acbe5374bf9ab1d10
Indykatz <cats83@me.com>
2022-07-14 21:48:41 +0100

    kat 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+// import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Login from "./pages/login";
+// import Home from "./pages/home";
+// import Feed from "./pages/feed";
+// import Profile from "./pages/profile";
 import "./App.css";
 
 const App = () => {
+  // const [user, setUser] = useState();
+
   return (
-    <div className="App">
-      <div>
-        <h1>Demo</h1>
-        <h1>Kat</h1>
-      </div>
-    </div>
+    <BrowserRouter className="App">
+      <Routes>
+        {/* element={<Login setter={setUser} user={user} />} */}
+        <Route path="/" />
+        {/* element={<Home user={user} setter={setUser} />} */}
+        <Route path="/home" />
+        {/* element={<Feed user={user} setter={setUser} />} */}
+        <Route path="/feed" />
+        {/* element={<Profile setter={setUser} user={user} />} */}
+        <Route path="/profile" />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
